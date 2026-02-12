fcab39f400b39df4628c10f53465afc78f0e89d1
julieza <juliezaraper@gmail.com>
2026-02-13 06:16:14 +0800

    Final Advertisement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
+          className="brightness-0 saturate-100 dark:brightness-0 dark:invert"
           src="/scals-logo.png"
           alt="SCALS Logo"
           width={100}
@@ -14,12 +14,15 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Comming Soon. Abangan next 100 years!
+            Coming Soon
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            SCALS Printing Services Agency and Transaction POS. WOW Sino ka dyan. KAY SCALS ANG 2026 💗 . 
+            SCALS Printing Services Agency Dashboard and Transaction POS. WOW Sino ka dyan.
           </p>
-          <p>IN JESUS NAME . WOHOOO !!!</p>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Abangan 3-5 years HAHAHA !  
+          </p>
+          <p>IN JESUS NAME . WOHOOO !!! KAY SCALS ANG 2026 💗 .</p>
         </div>
       </main>
     </div>
