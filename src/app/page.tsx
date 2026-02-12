d4f7cd56d7f098f3749c97538fc504fa0cc116d8
julieza <juliezaraper@gmail.com>
2026-02-13 06:23:06 +0800

    update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="brightness-0 saturate-100 dark:brightness-0 dark:invert"
-          src="/scals-logo.png"
+          src="/scals-logo.jpg"
           alt="SCALS Logo"
           width={100}
           height={20}
@@ -14,7 +13,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Coming Soon
+            Coming Soon 👕🖨️🪡
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             SCALS Printing Services Agency Dashboard and Transaction POS. WOW Sino ka dyan.
